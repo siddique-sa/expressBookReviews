c9d3f9f765e3ecc20aefd61f873e6829d954d399
siddique-sa <siddiqueahmed.sa@gmail.com>
2022-12-21 09:49:56 -0500

    aaded lines

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,94 @@ const regd_users = express.Router();
 
 let users = [];
 
+users.push({ username: 'John', password: '123456' });
+users.push({ username: 'Jane', password: 'password' });
+users.push({ username: 'Jack', password: 'qwerty' });
+
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //write code to check if username and password match the one we have in records.
+    return users.some(user => user.username === username && user.password === password);
+}
+
+const deleteReview = (isbn, username) => {
+    var reviewDeleted
+    for (const date in books[isbn].reviews) {
+        if (books[isbn].reviews[date].username === username) {
+          reviewDeleted = books[isbn].reviews[date];
+          delete books[isbn].reviews[date];
+        }
+    }
+    return reviewDeleted;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+
+    if(!isValid(username)){
+        return res.status(208).json({message: "Invalid Login. User '" + username + "' doesn't exist" });
+    }
+
+    if(!authenticatedUser(username,password)){
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+
+    let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+    console.log("Access token: " + accessToken);
+    req.session.authorization = {accessToken,username};
+    return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+regd_users.put("/auth/review/", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const user = req.session.authorization.username;
+  const isbn = req.body.isbn;
+  const review = req.body.review;
+  const currentDate = Date.now();
+  console.log("Usuario autenticado: " + user);
+  console.log("ISBN: " + isbn);
+  console.log("Texto review: " + review);
+  console.log("Timestamp: " + currentDate);
+
+  deleteReview(isbn,user);
+
+  books[isbn].reviews[currentDate] = {
+    username: user,
+    date: currentDate,
+    review: review
+  };
+
+  console.log(JSON.stringify(books[isbn]));
+  const newReview = books[isbn].reviews[currentDate];
+  return res.status(200).json({message: "Review added successfully", reviewAdded: newReview});
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    var reviewDeleted = deleteReview(isbn, username);
+
+    if(reviewDeleted === undefined){
+        return res.status(200).json({message: "No review to delete for user " + username + " in book " + isbn});
+    } else{
+        console.log(JSON.stringify(reviewDeleted));
+        return res.status(200).json({message: "Review deleted successfully", review: reviewDeleted});
+    }
+
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,115 @@
 const express = require('express');
+//import axios from 'axios';
+const axios = require('axios');
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //console.log(username);
+    //console.log(password);
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  //const filteredBooks = Object.keys(books)
+  //  .filter(key => key == isbn)
+  //  .map(key => books[key]);
+  const filteredBooks = books[isbn];
+  res.send(JSON.stringify(filteredBooks,null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+  res.send(JSON.stringify(filteredBooks,null,4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const filteredBooks = Object.values(books).filter(book => book.title === title);
+    res.send(JSON.stringify(filteredBooks,null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const filteredReviews = books[isbn].reviews;
+  res.send(JSON.stringify(filteredReviews,null,4));
+});
+
+//  Get book review
+public_users.get('/users/',function (req, res) {
+    //Write your code here
+    res.send(JSON.stringify(users,null,4));
+});
+
+public_users.get('/books-async', async function (req, res) {
+    const response = await axios.get('https://inakisanchez-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/');
+    const data = response.data;
+    //console.log("LogData: " + JSON.stringify(data, null, 4));
+    res.send(JSON.stringify(data,null,4));
+});
+
+public_users.get('/isbn-async/:isbn',async function (req, res) {
+    const isbn = req.params.isbn;
+    const response = await axios.get('https://inakisanchez-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/' + isbn);
+    const data = response.data;
+    //console.log("LogData: " + JSON.stringify(data, null, 4));
+    res.send(JSON.stringify(data,null,4));
+});
+
+public_users.get('/author-async/:author',async function (req, res) {
+    const author = req.params.author;
+    const response = await axios.get('https://inakisanchez-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/' + author);
+    const data = response.data;
+    //console.log("LogData: " + JSON.stringify(data, null, 4));
+    res.send(JSON.stringify(data,null,4));
+});
+
+public_users.get('/title-async/:title',async function (req, res) {
+    const title = req.params.title;
+    const response = await axios.get('https://inakisanchez-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/' + title);
+    const data = response.data;
+    //console.log("LogData: " + JSON.stringify(data, null, 4));
+    res.send(JSON.stringify(data,null,4));
 });
 
 module.exports.general = public_users;
